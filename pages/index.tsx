6421592b175f69d0bbd42022219c9da82a1832c6
Vignesh <reactivicky@gmail.com>
2023-03-04 16:30:40 +0530

    reduced margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,76 +3,74 @@ import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import {
-	Container,
-	Description,
-	DescriptionAnimation,
-	GetInTouchBtn,
-	Heading,
+  Container,
+  Description,
+  DescriptionAnimation,
+  GetInTouchBtn,
+  Heading,
 } from "../styles/Home.styled";
 import { HeadComponent } from "../components";
 
 const Home: NextPage = () => {
-	const router = useRouter();
-	const [word, setWord] = useState<string>("Web");
+  const router = useRouter();
+  const [word, setWord] = useState<string>("Web");
 
-	useEffect(() => {
-		const timer = setInterval(() => {
-			setWord((prevState) => {
-				if (prevState === "Web") {
-					return "React";
-				}
-				return "Web";
-			});
-		}, 2500);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setWord((prevState) => {
+        if (prevState === "Web") {
+          return "React";
+        }
+        return "Web";
+      });
+    }, 2500);
 
-		return () => {
-			clearInterval(timer);
-		};
-	}, []);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-	return (
-		<Container
-			className="container"
-			initial={{ opacity: 0, y: 20 }}
-			animate={{ opacity: 1, y: 0 }}
-			exit={{ opacity: 0, y: 20 }}
-			transition={{
-				bounceDamping: 1,
-			}}
-		>
-			<HeadComponent />
-			<div>
-				<Heading>
-					Vignesh <span>Kumar</span>
-				</Heading>
-				<Description>
-					<AnimatePresence exitBeforeEnter>
-						<DescriptionAnimation
-							key={word}
-							initial={{ y: 10, opacity: 0 }}
-							animate={{ y: 0, opacity: 1 }}
-							exit={{ y: -10, opacity: 0 }}
-						>
-							{word}
-						</DescriptionAnimation>
-					</AnimatePresence>
-					<span>Developer</span>
-				</Description>
-				<GetInTouchBtn
-					className="redBtn"
-					whileTap={{ scale: 0.8 }}
-					whileHover={{
-						color: 'black',
-						backgroundColor: 'white',
-						outline: '1px solid #eb4a4c'
-					}}
-					onClick={() => router.push("/contact")}
-				>
-					Get in Touch
-				</GetInTouchBtn>
-			</div>
-		</Container>
-	);
+  return (
+    <Container
+      className="container"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{
+        bounceDamping: 1,
+      }}
+    >
+      <HeadComponent />
+      <Heading>
+        Vignesh <span>Kumar</span>
+      </Heading>
+      <Description>
+        <AnimatePresence exitBeforeEnter>
+          <DescriptionAnimation
+            key={word}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+          >
+            {word}
+          </DescriptionAnimation>
+        </AnimatePresence>
+        <span>Developer</span>
+      </Description>
+      <GetInTouchBtn
+        className="redBtn"
+        whileTap={{ scale: 0.8 }}
+        whileHover={{
+          color: "black",
+          backgroundColor: "white",
+          outline: "1px solid #eb4a4c",
+        }}
+        onClick={() => router.push("/contact")}
+      >
+        Get in Touch
+      </GetInTouchBtn>
+    </Container>
+  );
 };
 
 export default Home;
